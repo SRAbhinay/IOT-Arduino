89bfd686157be98f69179be6ca5f9d1a37801bc6
S Raj Abhinay <124809897+SRAbhinay@users.noreply.github.com>
2023-10-21 11:27:44 +0530

    Update Code for Vehicle Indicator System.cpp

--- Code for Vehicle Indicator System.cpp
+++ Code for Vehicle Indicator System.cpp
@@ -5,7 +5,7 @@ void setup() {
 void loop() { 
 { digitalWrite(2, HIGH);
 delay(500);
-digitalWrite(2, LOW); // Turn off the LED
+digitalWrite(2, LOW);
 delay(500); // Delay for the same interval
  } }
 
