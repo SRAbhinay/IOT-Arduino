b94fdb2321bd56f8e5f4232a356641d9f8c8fe3b
S Raj Abhinay <124809897+SRAbhinay@users.noreply.github.com>
2023-10-21 11:20:12 +0530

    Update Arduino Bluetooth Module HC-05 for LED Blinking.cpp

--- Arduino Bluetooth Module HC-05 for LED Blinking.cpp
+++ Arduino Bluetooth Module HC-05 for LED Blinking.cpp
@@ -1,13 +1,21 @@
 int ledPin = 13;
+
 void setup() {
+
 pinMode(ledPin, OUTPUT);
 Serial.begin(9600);
-void loop() { if (Serial.available())
+
+void loop() { 
+if (Serial.available())
 { char command = Serial.read();
-if (receivedChar == 'LED ON') {
-digitalWrite(ledPin, HIGH);
-Serial.println("LED ON");
-} else if (receivedChar == 'LED OFF') {
-digitalWrite(ledPin, LOW);
-Serial.println("LED OFF");
-}
+  if (receivedChar == 'LED ON') {
+    digitalWrite(ledPin, HIGH);
+    Serial.println("LED ON");
+    } 
+  else if (receivedChar == 'LED OFF') {
+    digitalWrite(ledPin, LOW);
+    Serial.println("LED OFF");
+              }
+            }
+          }
+        }
