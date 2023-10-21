2fe4354b97327979ef1a25a533c4ebe3ce9b0fa7
S Raj Abhinay <124809897+SRAbhinay@users.noreply.github.com>
2023-10-21 11:25:40 +0530

    Update Arduino WIFI Module for LED blinking.cpp

--- Arduino WIFI Module for LED blinking.cpp
+++ Arduino WIFI Module for LED blinking.cpp
@@ -11,56 +11,56 @@ int status = WL_IDLE_STATUS;
 WiFiServer server(80);
 
 void setup() {
-Serial.begin(9600);
-pinMode(LED_BUILTIN, OUTPUT);
-while (status != WL_CONNECTED) {
-Serial.print("Attempting to connect to Network named: ");
-Serial.println(ssid);
-status = WiFi.begin(ssid, pass);
-delay(10000);
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to Network named: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
+    delay(10000);
+    }
+
+  server.begin();
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
 }
-server.begin();
-Serial.print("SSID: ");
-Serial.println(WiFi.SSID());
-IPAddress ip = WiFi.localIP();
-Serial.print("IP Address: ");
-Serial.println(ip);
-}
+
 void blinker() {
-Serial.println("Blinking");
-digitalWrite(LED_BUILTIN, HIGH);
-delay(1000); 
-digitalWrite(LED_BUILTIN, LOW); 
-delay(1000); 
+  Serial.println("Blinking");
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, LOW); 
+  delay(1000); 
 }
 
 void loop() {
-WiFiClient client = server.available();
-if (client) {
-Serial.println("new client");
-String currentLine = "";
-while (client.connected()) {
-if (client.available()) {
-char c = client.read();
-Serial.write(c);
-if (c == '\n') {
-if (currentLine.length() == 0) {
-client.println("Hello World");
-for(int i=0;i<10;i++)
-{
-blinker();
-}
-break;
-} else {
-currentLine = "";
-}
-} else if (c != '\r') {
-currentLine += c;
-}
-}
-}
-client.stop();
-Serial.println("client disonnected");
-}
-
+  WiFiClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    String currentLine = "";
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        if (c == '\n') {
+          if (currentLine.length() == 0) {
+            client.println("Hello World");
+            for(int i=0;i<10;i++){
+              blinker();
+                }
+              break;} 
+          else {
+            currentLine = "";
+          }} 
+        else if (c != '\r') {
+          currentLine += c;
+            }
+          }
+        }
+      client.stop();
+      Serial.println("client disonnected");
+    }
 }
